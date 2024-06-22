07abb5294696b6aa695b9b2df5fce9eb6fb3b7eb
akhdanrgya <akhdan.anargya@gmail.com>
2024-06-22 22:55:20 +0700

    Tab Bar Update

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,10 +1,32 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router'
+import TabBar from '../components/tabBar'
 
 const _layout = () => {
   return (
-    <Tabs/>
+    <Tabs
+    tabBar={props => <TabBar {...props} />}
+    >
+        <Tabs.Screen
+        name='home'
+        options={{
+            title : "Home"
+        }}
+        />
+        <Tabs.Screen
+        name='explore'
+        options={{
+            title : "Explore"
+        }}
+        />
+        <Tabs.Screen
+        name='profile'
+        options={{
+            title : "Profile"
+        }}
+        />
+    </Tabs>
   )
 }
 
